5fc3d2fe0690b7fac9e769210c43eed9b8ce3c53
ThibaultG1 <tiboyoyo@gmail.com>
2017-02-07 15:56:29 +0100

    suite

--- scrumtracker-web/src/main/java/hei/project/controller/HomeController.java
+++ scrumtracker-web/src/main/java/hei/project/controller/HomeController.java
@@ -1,7 +1,16 @@
 package hei.project.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by thiba on 07/02/2017.
  */
+@Controller
 public class HomeController {
+    @RequestMapping("/*")
+    public String goHome(){
+        return "Home";
+    }
 }
+
