9200c43fbf7aaf43380986912d9ec77933634b2b
anaickgithub <anaickm@gmail.com>
2017-02-20 11:22:22 +0100

    ihm home / sprint / story fini

---
+++ src/main/webapp/static/js/project.js
@@ -0,0 +1,8 @@
+function addNewSprint(){
+
+    var formaddnewsprint = document.getElementById("formaddnewsprint");
+    var newSprint = document.getElementById("newSprint");
+
+    formaddnewsprint.style.display = "block";
+    newSprint.style.display = "none";
+}
