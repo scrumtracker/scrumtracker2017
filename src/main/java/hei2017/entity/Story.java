2dc02b1bdd480bbb7eff3a38f4401c57469338d6
ThibaultG1 <tiboyoyo@gmail.com>
2017-03-10 13:58:40 +0100

    ajout de "Story points estimation"

--- src/main/java/hei2017/entity/Story.java
+++ src/main/java/hei2017/entity/Story.java
@@ -15,7 +15,7 @@ public class Story {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(unique=true)
+    @Column(unique = true)
     private Long id;
 
     private String nom;
@@ -23,6 +23,8 @@ public class Story {
     private String description;
 
     private StoryStatus status;
+
+    private Integer points;
 
     @JsonIgnore
     @ManyToOne(cascade = CascadeType.ALL)
@@ -33,27 +35,52 @@ public class Story {
     private Set<Task> storyTasks = new HashSet<Task>(0);
 
     //Constructeurs
-    public Story(){};
+    public Story() {
+    }
 
-    public Story(String nom){
+    ;
+
+    public Story(String nom) {
         this.nom = nom;
     }
 
     //Méthodes
-    public Long getId() { return id; }
+    public Long getId() {
+        return id;
+    }
 
-    public String getNom() { return nom; }
+    public String getNom() {
+        return nom;
+    }
 
-    public void setNom(String nom) { this.nom = nom; }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 
-    public String getDescription() { return description; }
+    public String getDescription() {
+        return description;
+    }
 
-    public void setDescription(String description) { this.description = description;}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
     @Enumerated(EnumType.STRING)
-    public StoryStatus getStatus() { return status; }
+    public StoryStatus getStatus() {
+        return status;
+    }
 
-    public void setStatus(StoryStatus status) { this.status = status; }
+    public void setStatus(StoryStatus status) {
+        this.status = status;
+    }
+
+    public Integer getPoints() {
+        return points;
+    }
+
+    public void setPoints(Integer points) {
+        this.points = points;
+    }
 
     public Sprint getStorySprint() {
         return storySprint;
@@ -63,8 +90,7 @@ public class Story {
         this.storySprint = storySprint;
     }
 
-    public void addTask(Task task)
-    {
+    public void addTask(Task task) {
         storyTasks.add(task);
     }
 }
