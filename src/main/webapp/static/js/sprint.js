b07038e9a70672cae8cdac347ec00a4bf24c16ee
PiSurQuatre <david.picart@outlook.fr>
2017-04-12 16:08:26 +0200

    Affichage des détails dune task fonctionnel

--- src/main/webapp/static/js/sprint.js
+++ src/main/webapp/static/js/sprint.js
@@ -1,3 +1,45 @@
+$(document).ready(function () {
+    $(".task").on("click", function(){
+        detailsTask(this.id);
+    })
+})
+
+//Affiche les détails d'une tâche
+function detailsTask( idTask)
+{
+    $.ajax({
+        url: '/api/task/'+idTask,
+        type: 'GET',
+        headers: {"Accept": "application/json", "Content-Type": "application/json"},
+        success: function (data) {
+            if(data.nom)
+                $("#taskDetails_nom").text(data.nom);
+            else
+                $("#taskDetails_nom").text("No name given to this task");
+            if(data.dateCreation)
+                $("#taskDetails_dateCreation").text(moment(data.dateCreation).format('DD/MM/YYYY HH:mm'));
+            else
+                $("#taskDetails_dateCreation").text("Not established");
+            if(data.tempsDeCharge)
+                if(data.uniteTempsDeCharge)
+                    $("#taskDetails_totalHoursOfWork").text(data.tempsDeCharge+" "+data.uniteTempsDeCharge);
+                else
+                    $("#taskDetails_totalHoursOfWork").text(data.tempsDeCharge+" au");
+            else
+                $("#taskDetails_totalHoursOfWork").text( "unknow");
+            if(data.description)
+                $("#taskDetails_description").text(data.description);
+            else
+                $("#taskDetails_description").text();
+
+        },
+        error: function (resultat, statut, erreur) {
+            toastr.error("An error occured. <br/>(" + statut + " - " + erreur + ").");
+        }
+
+    });
+}
+
 //Affiche le formulaire d'ajout de Story :
 function showFormAddNewStory(){
 
@@ -73,7 +115,8 @@ function addNewTaskInStory(idStory){
     task.nom = $("#newtaskname"+idStory).val();
     task.description = $("#newtaskdescription"+idStory).val();
     task.status = $("#newtaskstatus"+idStory).val();
-    task.tempsDeCharge = $("#newtaskTempsDeCharge"+idStory).val();
+    task.tempsDeCharge = $("#newtasktotalhoursofwork"+idStory).val();
+    task.dateCreation = Date.now();
     $.ajax({
         url: '/api/task/add/story/'+idStory,
         type: 'POST',
